75aa244bd705a2d808235891a6ec50e6d2541989
Cristian Beltran <3798796+cambel@users.noreply.github.com>
2026-03-18 17:47:33 +0900

    small fix?

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/CLAW/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/compliance-enabled-failure-recovery/' : './',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
